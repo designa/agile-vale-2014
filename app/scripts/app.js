a0fa7c8e00b9faa6eeeb8337d0e1f811c2cb58f1
Michel Allan <michel_allan20xx@hotmail.com>
2014-08-13 15:06:32 -0300

    estrutura inicial

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name agileVale2014App
+ * @description
+ * # agileVale2014App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('agileVale', [
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ]);
